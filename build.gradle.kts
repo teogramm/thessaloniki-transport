bf7bdb0f9b434f0a5ad2b4f01200d89ffa36de91
Theodoros Grammenos <teogramm@outlook.com>
2022-10-28 19:47:08 +0300

    Bump AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath(kotlin("gradle-plugin", version = "1.7.10"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
         // Android secrets module
